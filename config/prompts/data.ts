a1ab81ce51ae37ff9782700cf34f15c67e3f1907
bilalarslann <abilalbusiness@gmail.com>
2025-01-18 00:30:30 +0300

    refactor: move prompts to encrypted data file

---
+++ config/prompts/data.ts
@@ -0,0 +1,29 @@
+export const encryptedPrompts = {
+  "languageDetector": {
+    "en": "U2FsdGVkX19yISZjB/rQ0gcYfDC5oVa1ORdr1es+TTfCZdAPb8L3nbzYn/TOr04LCGH2kY60F7Srx9UrAunx4EZTV0XFhJoNiZbEsXafYeXUWn0Cw2w4Fu+u3npGwZqTu61nyz0PVFWA1wr2w0Lec0kHa6Z1QBY2gAcaHkwC2ihu5AW78955l4CSdSf2hCedWgLHnpZ7tOSjWmto7X6Stg==",
+    "tr": "U2FsdGVkX1+F9hxFapE4ECzxz7f6Y1+kyzHR90dbsABZRHS0F9RXYx91Q6GoZozR2o83CT1fEko3iv4EoGoCk2AsppHkkpSS0776CABirZRgt8E9vp8+H/LfQdKP3ddzKtbwhaYmrRMZZQAIrtyOMFwCr7DwvZFdeXegai5OjnPwPtLczaec6Udi5PFCN0MoXoR8Eg/Lb8Gf4zG3INeDhuvHtLhPE15WRaQd8GY5JcU="
+  },
+  "newsEditor": {
+    "en": "U2FsdGVkX19c8hgeWt/NuDt8i1M4IuYu4iesATRlno5aL21RRcukiTmaA59/e5I3UeGHuULhpMAJRugFZiTUYjPyyvVIQksX8X1XoH0abHe8lQOP+9HiaHj36HMBEleu+LMtHDhODAmiC0tHQIzAYOU9mb7htLBkJxl2/wFZG31frSc2DQ8Gb/qvn9zsFl2CplV4kQy7t0fIc3aY65klmdWJP39Ebsnt6M1xlbBDuA6xixt1AQO1oiZVTTmqvbqF",
+    "tr": "U2FsdGVkX1/WHQZUVLBHlZhDg/BJdG/INX86QfWIvybTCvjL4SGdNkI55YLzPkz/Hjp4TRzqBavr9boKvptq3M658bQeFQ8MEuYGcV6XZo0lcnK4uAh2Mzr1mHxtkh1VatsI5FZm+b8RzfoZcNZEPpBCFOxgaWitXw1FLRFajyJVc+2MQ3aZbnxgPPj9p8H9n/EzcY+Drd5nqVBCnHodmtw+mRJK9hRku88EQjUiyII5zugHghQ12V1tbmz9q5UOyTRUeeFxH2C+FFUSJ7qhXg=="
+  },
+  "journalist": {
+    "en": "U2FsdGVkX19mALrI4UKtK4WYjRGGeSPcoWya65+T5jpI/UrwPYOD2qc4gB6p3xpQ5hgJAGzpe6GY1Xhhido5ddc4B/ixuUVjIeZMpMCDuAae1HUAFe129dGh/mxJrr6dyDxUgvPI+rU0A3lAlk1SXvSM0vafQpyCse7Z7n+zfHOJD6t3kEXXytIQsxd3uJCavCRXn7RsJOKJEF2bfQntIQ==",
+    "tr": "U2FsdGVkX19q+tdLou9RAJfSCMcWxjq5rLz+vM3VJDe+ka26/cwQl620ofN82gniX1y7QD6oNR9weKiKaFxlHtOiDksI61rUh+KtgHCYQx4uahXr7GGMHj0mY3hUvXY1N5WRQgRo3wfrXHMaQRgIosp9MV67lJBVfKIORRmySgLATXLuPkkpw8f8rVrHXYDr"
+  },
+  "suggestions": {
+    "en": "U2FsdGVkX1+h+tDh2VeWe1aEENF/nkK6aaLkPoAg7amqyY2BTJ3U+rpgpB9S8H2ApC6BOmGw6e64jAXNKWUwCiohFPl2ECZNQSJRaEs9QeywPpVxvdiGHXq4/uumeHeXIwByOc+uEYHSVeNGbFAkipYl4dRfojGFeaCYK0fZmc6fO4Cju8a3ItURYKzc7lP7xG1ooL0aY58nWzo1UveafLxvcDaex1OlY6moA/on4LSDM2vz/eRKMzafhXZNqcIVwugcz6BV14wkyzJV9omG6g==",
+    "tr": "U2FsdGVkX18Q10KTMxAT5osV3MkvgnBC4SV6rRLMoY2m9nmRsKCTAjdRLbjOBMOVPRl3UrTdU/E0e3D5zpFC710RNvn9Cr6SM56vD8If2hOMp2zZfgtltE8H8HmDQX2XpIw5PtaqjdBV6ImRJ7V0nuzOp8kRA2uQP954KqeQ6F9EghAa6rqvaFnLbCozP7H30G98hNvBeP3B9vYszdnp14gumMW8jgrRvFTt9FwlnJs43HuGymxzPQ+uHoHY2IsV"
+  },
+  "translation": {
+    "en": "U2FsdGVkX19W7o2Nal9/24XqP5oVgXghTO/v0lz5pzHz+KHO+nJxzynBXvafn6glVgJhs3Ex0u6Nffrccmwyza3uIB+l9oL+fW6bsdmKqSrYQCmF7wezqOsE/EmIG9nSeUGOcrwfOxlQwKQ3OqVOPn6KkVMJIvbjVLa7XvVmTEvsj6uMqIij2KOJ7sb7iYK4MBtP8U/VOZy3C5W65UoP8xeQ4rAl9+WOXhH2BArF8CsKjWHpaoDqAOXhji5DI/NcAfuw4Xg/wv318jPheyxdPhKItIvL1aXNCIJZrk5S1XlVFss7+RNX22N7v7nS6xuN9BmEcHX2ucaETCKwINxEWIGOEhSnjZRy4Zq0JBZwxjQ=",
+    "tr": "U2FsdGVkX19tBym1QORk1khgeOsvbyK+WPx2G5ThGPF9/WZ3rOioL+/K6bTHAzm9OMoNrVdGNe1B8vjOaABehjqbToDGa+2hyIpIngR5h11+iWuqC4/QGkJjKfQBgSsIzv+2mnjg2BOjobdEjShYNxVqAkRVceHkNb/lZQ9Ffy5+YNXQEq062MOv4aP4l+dnOO1XFo+RFkJnR8P4IR8D2+Sv9JXg2uv3gbgCJmszlrgfVtHn4MM/q7o9Vhkmh6VeF0Vc2J8G4t/nzUi+oLN3h8gSEru5kolT7tVN4iadIHs2DLSb/usxF4uun1oV/2eLAb0xE2mdeZ57V230qWLw7D/WZZc8IrxaWdJNXearHSNj0RdpYEtZ+zP9bruCKA+ajOzX5ktDWGXaSj5AeZGd+g=="
+  },
+  "newsTranslation": {
+    "tr": "U2FsdGVkX1+iJ7PIRElk9Ax53bo6eouB6mK9kSnRTKTBCuK6IHWphW/2/xPItMD4wNi3NyYOKV+ehQhxKg15MPfm3/G+HiMxJ6fwh8+lzcf+WPmk971lDE4DvjMW4Nk+Odt/XuWzhcw04rLKyllUkCbALqP7JX5zc7g2CNkWyoDb0vStYYNOCLMeXfatR3bML1lGpfZwbfVlHZxmWBNQ+kz/x7f0qe/h9ISfoQSb43nI8Jv7MhSOi5kauwks2BGCZdlCP4UoO4uRyNm+4Ji7M7PHBrWDg8J7g5Kh+PulPeY="
+  },
+  "technicalAnalysis": {
+    "en": "U2FsdGVkX1/zs2ut4lEUvngru/MAOw/Jqgbb0gjKPyrEiy1eCBZV7ufTeyRgGkxfSokfbV2kU6uSBMF0pLt2KF5xR0wAax0a+fhDw1A2XWFuQU//i+/kVkOrDi3qm0YN8myjZ99xp0a94BvKmyxFu0pOcxdpwjvRDirfSWWBhgNBA3uy+1VsFZY6k5D/HjRmDL0hBQZ8nCOVwoWuItzn4A==",
+    "tr": "U2FsdGVkX18vlIOz823SzDPeF9Il9hGyqbzKtYX/f/qXxamzIjH4/eOVpNQ3SM9X9LcawAoMB3RsU3rf8LSrtcqydPEKNwd1t9FDRtB/f9q5ud+3NESsEUgUR4it860kL+xZmaX0lyz+r1MvZsfqvxx7l8hCIfrUcnfXRcUCTuZNkpces6Lm/vNd68rPRaaHf4ab3dXnh340iV0MQibCu9zp3DElQ5ICCR1x7bPManM="
+  }
+};
